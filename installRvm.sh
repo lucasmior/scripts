a4f169bcfc81e95a58e5f184111adf7102a2de49
Lucas <lucas.mior@gmail.com>
2016-05-16 14:02:38 -0300

    Added alternatives to install rvm

--- installRvm.sh
+++ installRvm.sh
@@ -3,9 +3,17 @@
 # install curl
 apt-get install curl -y
 
-# install rvm 
-curl -#LO https://rvm.io/mpapis.asc
-gpg --import mpapis.asc
+# install rvm alternative1
+#curl -#LO https://rvm.io/mpapis.asc
+#gpg --import mpapis.asc
+
+# install rvm alternative2
+#sudo chown -R $USER:$USER ~/.gnupg/
+#command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+#sudo chown -R $USER:$USER ~/.gnupg/
+
+# Gereric steps
 curl -L https://get.rvm.io | bash -s stable --ruby=2.1.5
 
+source /home/$USER/.rvm/scripts/rvm
 source /usr/local/rvm/scripts/rvm
